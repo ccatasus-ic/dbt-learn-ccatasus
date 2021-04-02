9c3c10501c4d2b16cd64774e8c17beb5eb8f3f17
Casey Catasus <casey.catasus@integraconnect.com>
2021-04-02 14:01:06 +0000

    Using sources

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -4,4 +4,4 @@ select  id as payment_id
         ,status
         ,amount/100::integer as amount
         ,created as created_at
-from    raw.stripe.payment
+from    {{ source('stripe','payment') }}
